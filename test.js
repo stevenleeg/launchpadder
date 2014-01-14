ac36cdc24df69890ff95dff0cd2dc3c933bd1a48
Steve Gattuso <stevenleeg@gmail.com>
2014-01-14 00:38:40 -0500

    Small fixes.

--- test.js
+++ test.js
@@ -1,9 +1,9 @@
 var launchpadder = require("./launchpadder").Launchpad;
-var color = require("./launchpadder").Color;
+var Color = require("./launchpadder").Color;
 
 // The 0 represents the MIDI port to connect with
 // The 1 represents the MIDI output-port to connect with
-var pad = new launchpadder(0, 1);
+var pad = new launchpadder(0, 0);
 
 pad.on("press", function (button) {
   // Ignore scene buttons
@@ -27,13 +27,13 @@ pad.on("release", function (button) {
 
 
 // Start blinking the first scene-button
-var state = color.GREEN;
+var state = Color.GREEN;
 var scene = pad.getButton(8, 0);
 scene.startBlinking(state);
 
-// Toggle blink color on press
+// Toggle blink Color on press
 scene.on("press", function (button) {
-  if (state == color.GREEN) {
+  if (state == Color.GREEN) {
     button.startBlinking(Color.YELLOW);
   } else {
     button.startBlinking(Color.GREEEN);
@@ -44,11 +44,6 @@ scene.on("press", function (button) {
 var scene2 = pad.getButton(8, 1);
 scene2.light(Color.AMBER);
 scene2.on("press", function (button) {
-  button.toggle(Color.AMBER, Color.RED);
+  button.toggle(Color.AMBER, color.RED);
 });
-
-
-
-
-
 
